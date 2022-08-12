4ea7aa2fd5d573d0af3b4018464c5d6b329b819e
TetianaMartyniuk <martyniuk.tanja@gmail.com>
2022-08-12 17:07:03 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const refs = {
+    btnStart: document.querySelector('button[data-start]'),
+    btnStop: document.querySelector('button[data-stop]'),
+    body: document.querySelector("body")
+};
 
+// const btnStart= document.querySelector('button[data-start]');
+// const btnStop= document.querySelector('button[data-stop]');
+// const body= document.querySelector("body");
+let timerId = null
+
+refs.btnStart.addEventListener("click", changeColor)
+refs.btnStop.addEventListener("click", noColor)
+
+
+function changeColor() {
+    timerId = setInterval(() => {
+        refs.body.style.backgroundColor = getRandomHexColor()
+    }, 1000)
+    refs.btnStart.disabled = true; 
+}
+
+
+function noColor() {
+    clearInterval(timerId);
+    refs.body.style.backgroundColor = "transparent";
+    refs.btnStart.disabled = false;
+}
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
